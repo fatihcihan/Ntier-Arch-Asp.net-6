2d08f2e9655e02f82a7e9b7eaf732ece426306ce
Fatih <fatihcihan666@gmail.com>
2022-08-06 13:12:22 +0300

    Core katmanina resultlar eklendi

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public interface IResult  // void dondurenler icin (true, eklendi) gibi bir bilgilendirme
+    {
+        bool Success { get; }
+        string Message { get; }
+
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        // propertyler getter (readonly) olmasina ragmen constructor'da set ettik 
+        public Result(bool success, string message) : this(success)     // this -> result class'ini temsil ediyor (tek parametreli constructor)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
