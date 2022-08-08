5f499402dd349fe987bccf4b4eb4d1c053c90296
Fatih <fatihcihan666@gmail.com>
2022-08-09 01:36:48 +0300

    core katmanina fluent validation eklendi, cross cutting concerns'de validation tool yapisi olusturuldu

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    // genellikle static olur (tek bir instance olusturur oradan devam edersin)
+    public static class ValidationTool
+    {
+        // kendi validation toolumuz
+        public static void Validate(IValidator validator, object entity)    // entity de olabilir dto da olabilir o yuzden object 
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+
+    }
+}
