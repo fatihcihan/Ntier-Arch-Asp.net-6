6a3a20efc085d47efafbc4f26913d79e299502a0
Fatih <fatihcihan666@gmail.com>
2022-08-16 16:22:42 +0300

    Business codelari business rules classinda yapilandirildi

---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,27 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        // params -> run icerisine parametre olarak istedigimiz kadar iresult verebiliyoruz
+        public static IResult Run(params IResult[] logics)     // is kurallarini (manager'da) calistirmak icin boyle bi class olusturduk
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)  // parametre ile gonderilen is kurallarindan basarisiz olani business'a haber ediyoruz
+                {                       
+                    return logic;
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
