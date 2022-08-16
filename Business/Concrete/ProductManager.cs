557086fe9f8eb6633bd5d08c1c9f9a8a12e7d0b4
Fatih <fatihcihan666@gmail.com>
2022-08-16 16:20:39 +0300

    Product manager ctor'da category service inject edildi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -20,10 +21,14 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {   // bir is sinifi baska siniflari new'lemez, injection eder, yani ne entity framework ne de in memory ismi gececek
         IProductDal _productDal;
-        public ProductManager(IProductDal productDal, ILogger logger)
+        //ICategoryDal _categoryDal;  bir manager kendi dal'i haricinde hicbir dal'i inject edemez ama service inject edbilir
+        ICategoryService _categoryService;
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             // burada constructor calisinca bizden bir product dal referansi istiyor ve biz de inject ettik
             _productDal = productDal;
+            _categoryService = categoryService;
+            //_categoryDal = categoryDal; 
         }
         // su an in memory kullaniyoruz daha sonra entity framework kullanabiliriz sonucta
 
@@ -66,17 +71,36 @@ namespace Business.Concrete
             //}
 
 
-            if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
-            {
-                // ayni isimde urun eklenemez 
-                if (CheckIfProductNameExists(product.Name).Success)
-                {
-                    _productDal.Add(product);
-                    return new SuccessResult(Messages.ProductAdded);
-                }
-            }
-            return new ErrorResult("");
+            //if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
+            //{
+            //    // ayni isimde urun eklenemez 
+            //    if (CheckIfProductNameExists(product.Name).Success)
+            //    {
+            //        _productDal.Add(product);
+            //        return new SuccessResult(Messages.ProductAdded);
+            //    }
+            //}
+            //return new ErrorResult();
 
+            // eger mevcut kategoris sayisi 15'i gectiyse sisteme yeni urun eklenemez (business code)
+
+            // yukarida check ile baslayan is kodlari hep iresult donduruyor, core'da bi calisma motoru yazip (Run) iresultlari ele alabiliriz
+            // bu yuzden core -> business -> business rules classina tasiyalim
+
+
+            IResult result = BusinessRules.Run(
+                CheckIfProductNameExists(product.Name),
+                CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+                CheckIfCategoryLimitExceded()
+                 );
+            // yukarida is kurallarini parametre olarak verdik ve calistirdik, eger tum kurallardan geciyorsa null'dir yoksa hata dondurecek
+            // ilerleyen zamanlarda tekrar bir is kurali yazacak olursan onu bir methoda cevirip direkt yukariya parametre olarak gonderebiliriz
+            if (result != null)
+            {
+                return result;
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -137,6 +161,18 @@ namespace Business.Concrete
             if (result)
             {
                 return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+        // eger mevcut kategori sayisi 15'i gectiyse sisteme yeni urun eklenemez (business code)
+        // product icin category service'i nasil yorumladigiyla alakali
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
             }
             return new SuccessResult();
         }
